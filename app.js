f8273e4dc35ae3f263dadc82ab99b7ea9b8f3003
kehinde yusuf <yusufkehindemubaraq@gmail.com>
2023-03-11 18:12:28 +0100

    Landing Page Set Up

---
+++ app.js
@@ -0,0 +1,7 @@
+function openSlideMenu(){
+    document.querySelector('.side-nav').style.width = '250px';
+ }
+
+ function closeSlideMenu(){
+    document.querySelector('.side-nav').style.width = '0';
+ }
